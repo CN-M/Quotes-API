33d1dc2916da76e8ef2eece187e536f2edd80a76
Ntsako Mbhalati <cn.mbhalati@gmail.com>
2022-02-08 15:00:21 +0200

    Finished Api

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const PORT = 8080;
+
+// Imports quotes.json file
+const quotes = require('./Quotes/quotes.json');
+
+// Generates random number and picks random quote
+function generateQuote() {
+    randomNumber = Math.floor(Math.random() * quotes.length);
+    return quotes[randomNumber]
+}
+
+app.listen(
+    PORT,
+    () => console.log(`Alive on Port: ${PORT}`)
+);
+
+// app.use( express.json() );
+
+app.get('/', (req, res) => {
+    res.status(200).send(
+        generateQuote()
+    )
+});
